92a6ece3af0c9e34dd3943a28da95fe83c38eb26
sgomez3 <165761491+sgomez3@users.noreply.github.com>
2024-06-14 09:50:02 -0300

    Update basepreguntas.js

--- juego-sofia/basepreguntas.js
+++ juego-sofia/basepreguntas.js
@@ -9,18 +9,18 @@ let basepreguntas = [
         pregunta: "¿Qué indica la señal que se presenta?",
         imagen: "https://i.ibb.co/88b6b4F/transitopesado.jpg",
         respuesta: "Tránsito pesado a la derecha",
-        distractores: ["Circulación exclusiva de camiones", "Sobrepasos por la derecha", "Ninguna es correcta"]
+        distractores: ["Circulación exclusiva de camiones", "Sobrepasos por la derecha", "Carril de emergencia para vehículos"]
     },
     {
         pregunta: "Determine qué representa esta señal:",
         imagen: "https://i.ibb.co/bRTwSbw/PEATON.jpg",
-        respuesta: "Cruce de peatones",
-        distractores: ["Cruce escolar", "Prohibición de cruzar la calzada para peatones", "Advertencia de senda peatonal a 500 metros."]
+        respuesta: "Advertencia de cruce de peatones",
+        distractores: ["Advertencia de cruce escolar", "Advertencia de inicio de maratón", "Advertencia de lugar para realizar ejercicios de caminata."]
     },
     {
         pregunta: "En circulación nocturna, ¿qué luces deben utilizarse?",
         imagen: "https://i.ibb.co/1QQzs2z/mandodeluces.jpg",
-        respuesta: "Las luces altas",
-        distractores: ["Las luces bajas", "Luces de posición", "Todas a la vez"]
+        respuesta: "Las luces bajas",
+        distractores: ["Las luces altas", "Luces de posición", "Todas a la vez"]
     }
 ];
