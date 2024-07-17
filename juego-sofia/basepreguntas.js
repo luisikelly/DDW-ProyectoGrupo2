2ee3b995c63a7807621eb8a1344a3f107aaffa73
sgomez3 <165761491+sgomez3@users.noreply.github.com>
2024-07-17 18:36:03 -0300

    Update basepreguntas.js

--- juego-sofia/basepreguntas.js
+++ juego-sofia/basepreguntas.js
@@ -1,25 +1,25 @@
 let basepreguntas = [
     {
         pregunta: "¿Qué significa esta demarcación amarilla en la calzada?",
-        imagen: "dobleamarilla.jpg",
+        imagen: "https://i.ibb.co/ZTWf7Sx/dobleamarilla.jpg",
         respuesta: "Indica, para ambos sentidos de circulación, que no debe ser traspasada ni se puede circular sobre ella",
         distractores: ["Señalización que divide carriles en la vía", "Circulación de solo vehículos particulares", "Ninguna es correcta"]
     },
     {
         pregunta: "¿Qué indica la señal que se presenta?",
-        imagen: "transitopesado.jpg",
+        imagen: "https://i.ibb.co/88b6b4F/transitopesado.jpg",
         respuesta: "Tránsito pesado a la derecha",
         distractores: ["Circulación exclusiva de camiones", "Sobrepasos por la derecha", "Ninguna es correcta"]
     },
     {
         pregunta: "Determine qué representa esta señal:",
-        imagen: "PEATON.jpg",
+        imagen: "https://i.ibb.co/bRTwSbw/PEATON.jpg",
         respuesta: "Cruce de peatones",
         distractores: ["Cruce escolar", "Prohibición de cruzar la calzada para peatones", "Advertencia de senda peatonal a 500 metros."]
     },
     {
         pregunta: "En circulación nocturna, ¿qué luces deben utilizarse?",
-        imagen: "mandodeluces.jpeg",
+        imagen: "https://i.ibb.co/1QQzs2z/mandodeluces.jpeg",
         respuesta: "Las luces bajas",
         distractores: ["Las luces altas", "Luces de posición", "Todas a la vez"]
     }
